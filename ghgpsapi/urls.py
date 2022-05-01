7a939464a7b18c624a3c853d598b1e0272be01be
EngineerDanny <agyapongdaniel7777@gmail.com>
2022-05-01 17:51:40 +0000

    add playground as another app

--- ghgpsapi/urls.py
+++ ghgpsapi/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from playground import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('test/', views.test, name='test'),
+    path('playground/', include('playground.urls')),
+]  
