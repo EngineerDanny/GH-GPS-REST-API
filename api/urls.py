5415557dbf4107ad83a78ca585d2fd52182668cd
EngineerDanny <agyapongdaniel7777@gmail.com>
2022-05-03 10:25:38 +0000

    write logic for getting GPS for latitude and longitude

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
       path('get-address/', views.get_address, name='get-address'),
+      path('get-gps/', views.get_gps, name='get_gps'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,3 @@
-from unicodedata import name
-from django.shortcuts import render
-from django.http import HttpResponse
-
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -10,42 +6,66 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+
 @api_view(['GET'])
 def get_address(request):
-
     # get name from query string
     name = request.GET.get('name')
 
     # encode name
     enc_name = requests.utils.quote(name)
-
     address_list = fetch_address_list(enc_name)
-
-    print(type(address_list))
 
     return Response(address_list)
 
 
+@api_view(['GET'])
+def get_gps(request):
+    # get latitude and longitude from query params
+    latitude = request.GET.get('lat')
+    longitude = request.GET.get('long')
+
+    # validate the latitude and longitude variables
+    if latitude is None or longitude is None:
+        return Response(data={'error': 'Request is missing latitude or longitude query parameters'},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    # check whether latitude and longitude are numbers
+    try:
+        float(latitude)
+        float(longitude)
+    except ValueError:
+        return Response(data={'error': 'Latitude and longitude must be numbers'},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    address_list = fetch_gps(latitude, longitude)
+
+    if address_list is None or len(address_list) == 0:
+        return Response(data={'status': 'No address found'},
+                        status=status.HTTP_204_NO_CONTENT)
+
+    return Response(
+        data={'status': 'Address found',
+              'address': address_list, 'count': len(address_list),
+              },
+        status=status.HTTP_200_OK)
+
 # get gps from address name
+# the address name can be the name or GPS address of the place
+
+
 def fetch_address_list(name):
-    base_url = 'https://ghanapostgps.com/'
     map_url = 'https://ghanapostgps.com/map/'
     action_url = 'https://ghanapostgps.com/jsWebCall.aspx?Action=GetLocation&GPSName=' + name
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'Connection': 'keep-alive'
-    }
     session = requests.Session()
     # session.headers.update(headers)
 
-    map_res = session.get(map_url, headers=headers, allow_redirects=True)
+    map_res = session.get(
+        map_url, headers=get_normal_headers(), allow_redirects=True)
 
     # get the value of the ASP.NET_SessionId from the map_res cookies
-    session_id = map_res.cookies.values()[0]
+    # session_id = map_res.cookies.values()[0]
 
     # find all the scripts under the body tag
     bs_body = BeautifulSoup(map_res.content, 'html.parser').body
@@ -53,12 +73,6 @@ def fetch_address_list(name):
 
     # find the script tag that contains the asaaseOwner token
     third_script = jscripts[2].contents[0]
-
-    # find the last script to extract a useful link
-    last_script = jscripts[-1]['src']
-
-    # append the last script to the base url
-    last_script_url = base_url + last_script
 
     asaase_user = "VGgxcyBJcyBvdXIgV2ViIFVzZXI="
     # get the asaaseOwner from the script
@@ -78,8 +92,51 @@ def fetch_address_list(name):
 
 
 # get gps from lat, long
+def fetch_gps(latitude, longitude):
+    map_url = 'https://ghanapostgps.com/map/'
+    action_url = 'https://ghanapostgps.com/jsWebCall.aspx?Action=GetGPSName&Lati={}&Longi={}'.format(
+        latitude, longitude)
 
-# get addressname from gps
+    session = requests.Session()
+    # session.headers.update(headers)
+
+    map_res = session.get(
+        map_url, headers=get_normal_headers(), allow_redirects=True)
+
+    # get the value of the ASP.NET_SessionId from the map_res cookies
+    # session_id = map_res.cookies.values()[0]
+
+    # find all the scripts under the body tag
+    bs_body = BeautifulSoup(map_res.content, 'html.parser').body
+    jscripts = bs_body.find_all('script')
+
+    # find the script tag that contains the asaaseOwner token
+    third_script = jscripts[2].contents[0]
+
+    # find the last script to extract a useful link
+    # last_script = jscripts[-1]['src']
+
+    asaase_user = "VGgxcyBJcyBvdXIgV2ViIFVzZXI="
+    # get the asaaseOwner from the script
+    asaase_owner = re.findall("\'([^']+)", third_script)[0]
+
+    # make a post request to get the address
+    address_res = session.post(action_url, data={'headers': {
+        "AsaaseOwner": asaase_owner,
+        "AsaaseUser": asaase_user,
+
+    }},
+        headers=get_address_headers(asaase_owner, asaase_user)
+    )
+
+    address_data = address_res.json()
+
+    try:
+        address_list = address_data['Table']
+    except Exception:
+        return None
+    # return the address list as json
+    return address_list
 
 
 # a helper function to get the address headers
@@ -93,5 +150,16 @@ def get_address_headers(asaase_owner, asaase_user):
         'Referer': 'https://ghanapostgps.com/map/',
         'Origin': 'https://ghanapostgps.com',
         'AsaaseOwner': asaase_owner,
-        'AsaaseUser': asaase_user
+        'AsaaseUser': asaase_user,
+
+    }
+
+
+def get_normal_headers():
+    return {
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Connection': 'keep-alive'
     }
