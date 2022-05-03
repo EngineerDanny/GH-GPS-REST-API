44713efe0fb8ed10505ca6f755afaad1aeadd895
EngineerDanny <agyapongdaniel7777@gmail.com>
2022-05-03 10:39:33 +0000

    major api view refactor

--- api/views.py
+++ api/views.py
@@ -12,11 +12,25 @@ def get_address(request):
     # get name from query string
     name = request.GET.get('name')
 
+    # validate the name variable
+    if name is None:
+        return Response(data={'error': 'Request is missing name query parameter'},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    # type check the name variable
+    try:
+        str(name)
+    except ValueError:
+        return Response(data={'error': 'name must be string'},
+                        status=status.HTTP_400_BAD_REQUEST)
+
     # encode name
     enc_name = requests.utils.quote(name)
-    address_list = fetch_address_list(enc_name)
+    action_url = 'https://ghanapostgps.com/jsWebCall.aspx?Action=GetLocation&GPSName=' + enc_name
 
-    return Response(address_list)
+    # get gps from address name
+    address_list = fetch_address(action_url)
+    return normal_response(address_list)
 
 
 @api_view(['GET'])
@@ -38,64 +52,18 @@ def get_gps(request):
         return Response(data={'error': 'Latitude and longitude must be numbers'},
                         status=status.HTTP_400_BAD_REQUEST)
 
-    address_list = fetch_gps(latitude, longitude)
-
-    if address_list is None or len(address_list) == 0:
-        return Response(data={'status': 'No address found'},
-                        status=status.HTTP_204_NO_CONTENT)
-
-    return Response(
-        data={'status': 'Address found',
-              'address': address_list, 'count': len(address_list),
-              },
-        status=status.HTTP_200_OK)
-
-# get gps from address name
-# the address name can be the name or GPS address of the place
-
-
-def fetch_address_list(name):
-    map_url = 'https://ghanapostgps.com/map/'
-    action_url = 'https://ghanapostgps.com/jsWebCall.aspx?Action=GetLocation&GPSName=' + name
-
-    session = requests.Session()
-    # session.headers.update(headers)
-
-    map_res = session.get(
-        map_url, headers=get_normal_headers(), allow_redirects=True)
-
-    # get the value of the ASP.NET_SessionId from the map_res cookies
-    # session_id = map_res.cookies.values()[0]
-
-    # find all the scripts under the body tag
-    bs_body = BeautifulSoup(map_res.content, 'html.parser').body
-    jscripts = bs_body.find_all('script')
-
-    # find the script tag that contains the asaaseOwner token
-    third_script = jscripts[2].contents[0]
-
-    asaase_user = "VGgxcyBJcyBvdXIgV2ViIFVzZXI="
-    # get the asaaseOwner from the script
-    asaase_owner = re.findall("\'([^']+)", third_script)[0]
-
-    # make a post request to get the address
-    address_res = session.post(action_url, data={'headers': {
-        "AsaaseOwner": asaase_owner,
-        "AsaaseUser": asaase_user
-    }, }, headers=get_address_headers(asaase_owner, asaase_user))
-
-    address_data = address_res.json()
-    address_list = address_data['Table']
-
-    # return the address list as json
-    return address_list
-
-
-# get gps from lat, long
-def fetch_gps(latitude, longitude):
-    map_url = 'https://ghanapostgps.com/map/'
     action_url = 'https://ghanapostgps.com/jsWebCall.aspx?Action=GetGPSName&Lati={}&Longi={}'.format(
         latitude, longitude)
+
+    # the address name can be the name or GPS address of the place
+    address_list = fetch_address(action_url)
+
+    return normal_response(address_list)
+
+
+
+def fetch_address(action_url):
+    map_url = 'https://ghanapostgps.com/map/'
 
     session = requests.Session()
     # session.headers.update(headers)
@@ -163,3 +131,14 @@ def get_normal_headers():
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
         'Connection': 'keep-alive'
     }
+
+
+def normal_response(address_list):
+    if address_list is None or len(address_list) == 0:
+        return Response(data={'status': 'No address found'},
+                        status=status.HTTP_204_NO_CONTENT)
+    return Response(
+        data={'status': 'Address found',
+              'address': address_list, 'count': len(address_list),
+              },
+        status=status.HTTP_200_OK)
